5344838004c3f1d910272c6b586cbb1cccd4d45f
Mathieu L <LangloisMathieuPro@gmail.com>
2016-03-14 15:55:02 +0100

    Creation dans mypdo méthodes

---
+++ Liemie/class/Patient.class.php
@@ -0,0 +1,39 @@
+<?php
+Class Patient {
+	
+	public $id_patient;
+	public $email;
+	public $nom;
+	public $prenom;
+	public $adresse_num;
+	public $adresse_rue;
+	public $adresse_ville;
+	public $adresse_cp;
+	public $tel1;
+	public $tel2;
+	public $tel3;
+	public $date_naiss;
+	public $lien_photo;
+	public $id_adresse;
+	
+	/*public function __construct ($Id, $Email, $Nom, $Prenom, $Adr_num, $Adr_rue, $Adr_ville, $Adr_cp, $Tel1, $Tel2, $Tel3, $Date_naiss, $Lien_photo)
+	{
+		$this->id_patient = $Id;
+		$this->email = $Email;
+		$this->nom = $Nom;
+		$this->prenom = $Prenom;
+		$this->adresse_num = $Adr_num;
+		$this->adresse_ruee = $Adr_rue;
+		$this->adresse_ville = $Adr_ville;
+		$this->adresse_cp = $Adr_cp;
+		$this->tel1 = $Tel1;
+		$this->tel2 = $Tel2;
+		$this->tel3 = $Tel3l;
+		$this->date_naiss = $Date_naissate;
+		$this->lien_photo = $Lien_photo;
+	}*/
+	
+	public function __construct() {
+		
+	}
+}
