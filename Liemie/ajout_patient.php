75f99259dca8bb7e8008fa0ccbbe9980fedea610
Mathieu L <LangloisMathieuPro@gmail.com>
2016-04-18 17:01:04 +0200

    Modif MYPDO PATIENT

--- Liemie/ajout_patient.php
+++ Liemie/ajout_patient.php
@@ -9,7 +9,7 @@ $site->js = 'jquery.tooltipster.min';
 $site->css = 'tooltipster';
 $site->css = 'modal';
 
-$controleur = new controleur ();
+$controleur = new controleur_patient();
 
 $site->right_sidebar = $site->rempli_right_sidebar ();
 $site->left_sidebar = $controleur->retourne_formulaire_patient ( 'Ajout' );
